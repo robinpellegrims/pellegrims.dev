b47144efa39f5c35f69da25ca563cbf949086ddb
Robin Pellegrims <robin@pellegrims.dev>
2026-02-13 13:59:30 +0100

    Update Next.js to pinned release

--- libs/ui/atoms/src/lib/counter-dev-analytics/counter-dev-analytics.tsx
+++ libs/ui/atoms/src/lib/counter-dev-analytics/counter-dev-analytics.tsx
@@ -9,6 +9,6 @@ export const CounterDevAnalytics: FunctionComponent<
   CounterDevAnalyticsProps
 > = ({ user }) => (
   <Script id="analytics-script" strategy="lazyOnload">
-    {`if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"${user}",utcoffset:"1"}))};sessionStorage.setItem("_swa","1");`}
+    {`if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"${user}",utcoffset:"1"})).catch(()=>{})};sessionStorage.setItem("_swa","1");`}
   </Script>
 );
