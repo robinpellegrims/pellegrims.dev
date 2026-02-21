62a25993287bbdeca4c06240bc014ca86e7e11c9
Robin Pellegrims <robin@pellegrims.dev>
2026-02-21 13:14:56 +0100

    feat: favicon improvement

--- libs/ui/atoms/src/lib/favicon/favicon.tsx
+++ libs/ui/atoms/src/lib/favicon/favicon.tsx
@@ -1,5 +1,7 @@
 import { FunctionComponent } from 'react';
 
+const faviconVersion = '20260221';
+
 export const FavIcon: FunctionComponent<{ pathPrefix?: string }> = ({
   pathPrefix,
 }) => (
@@ -7,26 +9,32 @@ export const FavIcon: FunctionComponent<{ pathPrefix?: string }> = ({
     <link
       rel="apple-touch-icon"
       sizes="180x180"
-      href={`${pathPrefix}/apple-touch-icon.png`}
+      href={`${pathPrefix}/apple-touch-icon.png?v=${faviconVersion}`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="32x32"
-      href={`${pathPrefix}/favicon-32x32.png`}
+      href={`${pathPrefix}/favicon-32x32.png?v=${faviconVersion}`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="16x16"
-      href={`${pathPrefix}/favicon-16x16.png`}
+      href={`${pathPrefix}/favicon-16x16.png?v=${faviconVersion}`}
     />
-    <link rel="manifest" href={`${pathPrefix}/site.webmanifest`} />
-    <link rel="shortcut icon" href={`${pathPrefix}/favicon.ico`} />
+    <link
+      rel="manifest"
+      href={`/site.webmanifest?v=${faviconVersion}`}
+    />
+    <link
+      rel="shortcut icon"
+      href={`${pathPrefix}/favicon-32x32.png?v=${faviconVersion}`}
+    />
     <meta name="msapplication-TileColor" content="#da532c" />
     <meta
       name="msapplication-config"
-      content={`${pathPrefix}/browserconfig.xml`}
+      content={`${pathPrefix}/browserconfig.xml?v=${faviconVersion}`}
     />
     <meta name="theme-color" content="#ffffff" />
   </>
