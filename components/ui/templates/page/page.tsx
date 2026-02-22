18747c6598669870ea52bd9d892ba9411fe9a545
Robin Pellegrims <robin@pellegrims.dev>
2026-02-22 16:51:27 +0100

    feat: remove card wrapper

--- components/ui/templates/page/page.tsx
+++ components/ui/templates/page/page.tsx
@@ -18,12 +18,9 @@ export const PageTemplate: FunctionComponent<PageTemplateProps> = ({
     <NextSeo {...seoProps} />
     <section className="relative pb-20 pt-6 sm:pt-10">
       <Container>
-        <div className="relative overflow-hidden rounded-[2rem] border border-ink/10 bg-white/70 p-5 shadow-[0_20px_80px_-65px_rgba(31,33,38,0.9)] backdrop-blur sm:p-8 lg:p-10">
-          <div className="pointer-events-none absolute -right-24 top-0 h-64 w-64 rounded-full bg-accent/10 blur-3xl" />
-          <div className="relative">
-            {header}
-            {children}
-          </div>
+        <div className="relative">
+          {header}
+          {children}
         </div>
       </Container>
     </section>
