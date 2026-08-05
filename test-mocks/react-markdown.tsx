2e84384858cd749f14f455df2b4a43914bd96f7e
Robin Pellegrims <6053571+robinpellegrims@users.noreply.github.com>
2026-08-05 21:30:24 +0200

    test: mock react-markdown in Jest

---
+++ test-mocks/react-markdown.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from 'react';
+
+type ReactMarkdownProps = { children?: ReactNode };
+
+const ReactMarkdown = ({ children }: ReactMarkdownProps) => <>{children}</>;
+
+export default ReactMarkdown;
